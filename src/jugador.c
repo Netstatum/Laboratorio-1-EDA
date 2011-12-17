dbed9fa00ccf6eb24109bbe231530d812d2d8f7e
Felipe Garay <netstatum@gmail.com>
2011-12-17 17:06:14 -0300

    Primer commit de la lista enlazada circularmente
    Se incluyen las siguientes funciones:
    siguiente_jugador: que devuelve el jugador al siguiente turno
    agregar_jugador: que agrega un jugador a la lista dada o crea una nueva lista
    test_jugador: para probar el codigo de la lista

--- src/jugador.c
+++ src/jugador.c
@@ -1,13 +1,13 @@
 #include "jugador.h"
 #include <malloc.h>
 
-Jugador *siguiente_jugador(Jugador *actual);
+Jugador *siguiente_jugador(Jugador *actual)
 {
 	return actual->siguiente;
 }
 
 
-Jugador *agregar_jugador(Jugador *final, unsigned int monto_actual);
+Jugador *agregar_jugador(Jugador *final, unsigned int monto_actual)
 {
 	Jugador *nuevo=NULL, *aux=NULL;
 
@@ -47,3 +47,62 @@ Jugador *agregar_jugador(Jugador *final, unsigned int monto_actual);
 		}
 	}
 }
+
+#ifdef DEBUG
+int test_jugador()
+{
+	/*Vemos si podemos crear un jugador en una lista vacia*/
+	Jugador *jugador=agregar_jugador(NULL, 2);
+
+	if(!jugador)
+	{
+		return -1;
+	}
+
+	if(jugador->monto_actual!=2)
+	{
+		return -1;
+	}
+
+	/*Vemos si se agrega correctamente un nuevo jugador*/
+	jugador=agregar_jugador(jugador, 3);
+
+	if(!jugador)
+	{
+		return -1;
+	}
+
+	if(jugador->monto_actual!=3)
+	{
+		return -1;
+	}
+	
+	/*Deberia ser el primer elemento*/
+	jugador=siguiente_jugador(jugador);
+
+	if(!jugador)
+	{
+		return -1;
+	}
+
+	if(jugador->monto_actual!=2)
+	{
+		return -1;
+	}
+	
+	jugador=siguiente_jugador(jugador);
+	/*Deberia ser el segundo elemento*/
+
+	if(!jugador)
+	{
+		return -1;
+	}
+
+	if(jugador->monto_actual!=3)
+	{
+		return -1;
+	}
+
+	return 0; /*Todo salio bien :)*/
+}
+#endif
--- src/jugador.h
+++ src/jugador.h
@@ -1,4 +1,6 @@
 /*jugador.h
+ * Para ejemplos de uso vease la funcion test_jugador
+ *
  * Las siguientes funciones se encuentran en este archivo.
  *
  * siguiente_jugador: Devuelve el jugador al cual le corresponde el siguiente
@@ -11,7 +13,7 @@
 typedef struct Jugador{
 	
 	unsigned int monto_actual;
-	Jugador *siguiente;
+	struct Jugador *siguiente;
 } Jugador;
 
 
@@ -33,3 +35,11 @@ Jugador *siguiente_jugador(Jugador *actual);
  * RETORNO: El jugador que se agrega luego de usar la funcion. Devuelve NULL si
  * 	fallamos al obtener memoria para el nuevo nodo*/
 Jugador *agregar_jugador(Jugador *final, unsigned int monto_actual);
+
+
+#ifdef DEBUG
+/*Ejecuta algunas pruebas para ver que el codigo de jugador funciona. Solo esta
+ * disponible si se especifica DEBUG
+ * RETORNO: 0 Si todo salio bien*/
+int test_jugador();
+#endif
