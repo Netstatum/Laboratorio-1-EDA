afd29bba1e9e1016784ccb7fa9a00e2e8d990490
Felipe Garay <netstatum@gmail.com>
2011-12-23 00:39:41 -0300

    Se terminan las funciones de ascii art
    Ahora se pueden crear todas las cartas en ascii art para mostrarlas con las
    funciones de display

--- src/ascii.c
+++ src/ascii.c
@@ -1,30 +1,90 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "ascii.h"
 
-const char **carta_a_ascii(CARTA carta)
+char *carta_a_ascii(CARTA carta, unsigned int linea)
 {
 	switch(carta.pinta)
 	{
 		case 3:
 			/*Corazones*/
-			switch(carta.valor)
+			if(carta.valor>0 && carta.valor<11)
 			{
-				case 2:
-					return corazones_2();
-					break;
-				default:
-					return NULL;
-					break;
+				/*le sumamos 48 para obtener el valor ascii del
+				 * numero*/
+				return corazones(linea, carta.valor+48);
+			}else{
+				/*No son numeros, son letras*/
+				switch(carta.valor)
+				{
+					case 11:
+						return corazones(linea, 'J');
+					case 12:
+						return corazones(linea, 'Q');
+					case 13:
+						return corazones(linea, 'K');
+				}
 			}
 			break;
 		case 4:
 			/*diamante*/
+			if(carta.valor>0 && carta.valor<11)
+			{
+				/*le sumamos 48 para obtener el valor ascii del
+				 * numero*/
+				return diamante(linea, carta.valor+48);
+			}else{
+				/*No son numeros, son letras*/
+				switch(carta.valor)
+				{
+					case 11:
+						return diamante(linea, 'J');
+					case 12:
+						return diamante(linea, 'Q');
+					case 13:
+						return diamante(linea, 'K');
+				}
+			}
 			break;
 		case 5:
 			/*trebol*/
+			if(carta.valor>0 && carta.valor<11)
+			{
+				/*le sumamos 48 para obtener el valor ascii del
+				 * numero*/
+				return trebol(linea, carta.valor+48);
+			}else{
+				/*No son numeros, son letras*/
+				switch(carta.valor)
+				{
+					case 11:
+						return trebol(linea, 'J');
+					case 12:
+						return trebol(linea, 'Q');
+					case 13:
+						return trebol(linea, 'K');
+				}
+			}
 			break;
 		case 6:
 			/*pica*/
+			if(carta.valor>0 && carta.valor<11)
+			{
+				/*le sumamos 48 para obtener el valor ascii del
+				 * numero*/
+				return pica(linea, carta.valor+48);
+			}else{
+				/*No son numeros, son letras*/
+				switch(carta.valor)
+				{
+					case 11:
+						return pica(linea, 'J');
+					case 12:
+						return pica(linea, 'Q');
+					case 13:
+						return pica(linea, 'K');
+				}
+			}
 			break;
 
 		default:
@@ -64,44 +124,194 @@ return "        _..-''--'----_.                                        \n"
 " `-------'                          `---'                               \n";
 }
 
-const char **corazones_2()
+char *corazones(unsigned int linea, char caracter)
 {
-	const char **cadena={
-		{".------."},
-		{"|2_  _ |"},
-		{"|( \/ )|"},
-		{"| \  / |"},
-		{"|  \/ 2|"},
-		{"`-----+'"}
-	};
+	/*Estamos practicamente obligados a usar punteros ya que las variables
+	 * locales se eliminan al dejar de usar la funcion*/
+	char *cadena=malloc(sizeof(char)*8);
+
+	switch(linea)
+	{
+		case 0:
+			sprintf(cadena, ".------.");
+			break;
+		case 1:
+			sprintf(cadena, "|%c_  _ |", caracter);
+			break;
+		case 2:
+			sprintf(cadena, "|( \\/ )|");
+			break;
+		case 3:
+			sprintf(cadena, "| \\  / |");
+			break;
+		case 4:
+			sprintf(cadena, "|  \\/ %c|", caracter);
+			break;
+		case 5:
+			sprintf(cadena, "`-----+'");
+			break;
+	}
 
 	return cadena;
 }
 
-char cartas_5_graficas(CARTA cartas[])
+  
+char *diamante(unsigned int linea, char caracter)
 {
-	char i, l;
+	/*Estamos practicamente obligados a usar punteros ya que las variables
+	 * locales se eliminan al dejar de usar la funcion*/
+	char *cadena=malloc(sizeof(char)*8);
 
-	/*Una carta ocupa 48 caracteres . Son 5 cartas asi que es 48*5=240
-	 * Ademas debemos poner 6 caracteres de espacio ademas del salto de
-	 * linea entre cartas 6*5+240=270 y agregarle un caracter final NULL
-	 * quedando como total 271*/
-
-	char cadena[271]={NULL}; 
-
-	/*Aqui vamos guardand cada linea para luego usarla asi:
-	 * [numero de carta][linea][contenido_linea]*/
-	char lineas[5][6][8]={NULL};
-
-
-	
-	for(i=0;i<5;i++)
+	switch(linea)
 	{
-		for(l=0;l<6;l++)
-		{
-			sprintf(lineas[i][l], "%s", carta_a_ascii(cartas[i]));
-			printf("%s\n", lineas[i][l]);
-		}
+		case 0:
+			sprintf(cadena, ".------.");
+			break;
+		case 1:
+			sprintf(cadena, "|%c /\\  |", caracter);
+			break;
+		case 2:
+			sprintf(cadena, "| /  \\ |");
+			break;
+		case 3:
+			sprintf(cadena, "| \\  / |");
+			break;
+		case 4:
+			sprintf(cadena, "|  \\/ %c|", caracter);
+			break;
+		case 5:
+			sprintf(cadena, "`-----+'");
+			break;
 	}
 
+	return cadena;
+}
+
+char *trebol(unsigned int linea, char caracter)
+{
+	/*Estamos practicamente obligados a usar punteros ya que las variables
+	 * locales se eliminan al dejar de usar la funcion*/
+	char *cadena=malloc(sizeof(char)*8);
+
+	switch(linea)
+	{
+		case 0:
+			sprintf(cadena, ".------.");
+			break;
+		case 1:
+			sprintf(cadena, "|%c _   |", caracter);
+			break;
+		case 2:
+			sprintf(cadena, "| ( )  |");
+			break;
+		case 3:
+			sprintf(cadena, "|(_x_) |");
+			break;
+		case 4:
+			sprintf(cadena, "|  Y  %c|", caracter);
+			break;
+		case 5:
+			sprintf(cadena, "`-----+'");
+			break;
+	}
+
+	return cadena;
+
+}
+
+
+
+
+char *pica(unsigned int linea, char caracter)
+{
+	/*Estamos practicamente obligados a usar punteros ya que las variables
+	 * locales se eliminan al dejar de usar la funcion*/
+	char *cadena=malloc(sizeof(char)*8);
+
+	switch(linea)
+	{
+		case 0:
+			sprintf(cadena, ".------.");
+			break;
+		case 1:
+			sprintf(cadena, "|%c .   |", caracter);
+			break;
+		case 2:
+			sprintf(cadena, "| / \\  |");
+			break;
+		case 3:
+			sprintf(cadena, "|(_,_) |");
+			break;
+		case 4:
+			sprintf(cadena, "|  I  %c|", caracter);
+			break;
+		case 5:
+			sprintf(cadena, "`-----+'");
+			break;
+	}
+
+	return cadena;
+
+
+}
+
+char *cartas_5_graficas(CARTA cartas[])
+{
+	int l;
+	char *cadena=malloc(sizeof(char)*276);
+
+	/*5 cartas de 8 caracteres +4 espacios +1 salto de linea +NULL*/
+	char lineas[6][5*8+6+17]={'\0'};
+	char *carta1,*carta2,*carta3,*carta4,*carta5;
+	
+	for(l=0;l<6;l++)
+	{
+		carta1=carta_a_ascii(cartas[0], l);
+		carta2=carta_a_ascii(cartas[1], l);
+		carta3=carta_a_ascii(cartas[2], l);
+		carta4=carta_a_ascii(cartas[3], l);
+		carta5=carta_a_ascii(cartas[4], l);
+
+		/*vamos guardando linea por linea para luego contatenarla*/
+		/*debemos dejar un espacio al principio de 17 caracteres por (80-(5*8+6))/2*/
+		sprintf(lineas[l], "                 %s %s %s %s %s\n", carta1,carta2,carta3,carta4,carta5);
+
+		free(carta1);
+		free(carta2);
+		free(carta3);
+		free(carta4);
+		free(carta5);
+	}
+
+	/*concatenamos todo en un mismo string*/
+	sprintf(cadena, "%s%s%s%s%s%s", lineas[0], lineas[1], lineas[2], lineas[3], lineas[4], lineas[5]);
+
+	return cadena;
+}
+
+char *cartas_2_graficas(CARTA cartas[])
+{
+	int l;
+	char *cadena=malloc(sizeof(char)*132);
+
+	/*2 cartas de 8 caracteres +1 espacios +1 salto de linea +NULL*/
+	char lineas[6][2*8+3]={'\0'};
+	char *carta1,*carta2;
+	
+	for(l=0;l<6;l++)
+	{
+		carta1=carta_a_ascii(cartas[0], l);
+		carta2=carta_a_ascii(cartas[1], l);
+
+		/*vamos guardando linea por linea para luego contatenarla*/
+		sprintf(lineas[l], "%s %s\n", carta1,carta2);
+
+		free(carta1);
+		free(carta2);
+	}
+
+	/*concatenamos todo en un mismo string*/
+	sprintf(cadena, "%s%s%s%s%s%s", lineas[0], lineas[1], lineas[2], lineas[3], lineas[4], lineas[5]);
+
+	return cadena;
 }
--- src/ascii.h
+++ src/ascii.h
@@ -7,9 +7,33 @@
 
 #include "carta.h"
 
-const char **carta_a_ascii(CARTA carta);
+/*Devuelve la linea indicada (entre 0 y 5) del string que representa la carta
+ * ARGUMENTOS:
+ * 	carta: carta a mostrar
+ * 	linea: la linea que se quiere*/
+char *carta_a_ascii(CARTA carta, unsigned int linea);
 
-const char **corazones_2();
+/*Las siguientes funciones devuelven la linea del string que representa a la
+ * carta con el caracter dado en la parte del valor
+ * ARGUEMNTOS:
+ * 	linea: La linea que se quiere
+ * 	caracter: el caracter a poner en la carta */
+char *corazones(unsigned int linea, char caracter);
+char *diamante(unsigned int linea, char caracter);
+char *trebol(unsigned int linea, char caracter);
+char *pica(unsigned int linea, char caracter);
+
+/*Devuelve el mensaje de bienvenida del programa*/
 const char *mensaje_bienvenida();
+
+/*Devuelve un string que representa a las 2 cartas dadas como array
+ * ARGUMENTOS:
+ * 	cartas: array de cartas de tamano 2*/
+char *cartas_2_graficas(CARTA cartas[]);
+
+/*Devuelve un string que representa a las 5 cartas dadas como array
+ * ARGUMENTOS:
+ * 	cartas: array de cartas de tamano 5*/
+char *cartas_5_graficas(CARTA cartas[]);
 
 #endif
