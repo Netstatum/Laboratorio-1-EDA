424f9fd930438dbcf112f07730ae387f8a61eb12
Felipe Garay <netstatum@gmail.com>
2011-12-23 00:40:39 -0300

    Se crean las funciones para mostrar informacion
    Aun no estan terminadas estas funciones

---
+++ src/display.h
@@ -0,0 +1,11 @@
+#ifndef _H_DISPLAY_
+#define _H_DISPLAY_
+
+#include "carta.h"
+
+
+/*Formatea y muestra las variables dadas segun lo especificado en el lab*/
+/*TODO: Falta agregar las probabilidades*/
+void display_principal(CARTA cartas_mesa[], CARTA cartas_jugador[]);
+
+#endif
