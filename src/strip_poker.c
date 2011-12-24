c4153138934e245c1856b188a37ab54223e7a348
Esteban Gaete <zoraskiller@gmail.com>
2011-12-24 02:02:18 -0300

    con menuPrincipal desactivado, probando crearJugadores... funcionaaa

--- src/strip_poker.c
+++ src/strip_poker.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "menu.h"
+#include "jugador.h"
 
 int main()
 {
+    Jugador *jugadoresMesa;
+
     printf("%s",mensajeBienvenida());
     printf("\n\n\n\n\n\t\tANTES DE COMENZAR A JUGAR AGRANDE EL TAMA%cO DE LA CONSOLA",165);
     printf("\n\n\t\t\t\tAL MAXIMO POSIBLE\n");
 
+    jugadoresMesa = crearJugadores(5);
+
     getch();
-    menuPrincipal(0);
+
+
+    //menuPrincipal(0);
 }
