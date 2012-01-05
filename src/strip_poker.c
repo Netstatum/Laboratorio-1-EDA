c62702fcf5f04a852d0d944eff74f122c03da72a
Esteban Gaete <zoraskiller@gmail.com>
2012-01-05 17:32:29 -0300

    tal cual esta es como debe ser :P

--- src/strip_poker.c
+++ src/strip_poker.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
-#include <malloc.h>
 #include "getch.h"
 #include "menu.h"
-#include "jugador.h"
 
 int main()
 {
-    Jugador *jugadoresMesa;
+
 
     printf("%s",mensajeBienvenida());
     printf("\n\n\n\n\n\t\tANTES DE COMENZAR A JUGAR AGRANDE EL TAMA%cO DE LA CONSOLA",165);
     printf("\n\n\t\t\t\tAL MAXIMO POSIBLE\n");
-
-    jugadoresMesa = crearJugadores(5);
-
     getch();
 
-
-    //menuPrincipal(0);
-
+    menuPrincipal(0);
 
     return 0;
 }
