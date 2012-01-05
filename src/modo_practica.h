d618a946de2393e005ebb8dce93f817eb391f4c6
Esteban Gaete <zoraskiller@gmail.com>
2012-01-05 17:30:41 -0300

    como todo header hace lo suyo~

---
+++ src/modo_practica.h
@@ -0,0 +1,18 @@
+#ifndef MODO_PRACTICA_H_INCLUDED
+#define MODO_PRACTICA_H_INCLUDED
+
+/** Función modo_practica, esta función es la encargada de inicializar la partida
+    para el modo practica y lanzar el juego
+*/
+int modo_practica();
+
+/** Preguntara al usuario la cantidad de jugadores involucrados en la partida
+    Y el monto inicial de cada uno de ellos
+*/
+void cantidad_jugadores();
+
+/** Entra la informacion sobre algunos aspectos basicos para tener en cuenta al jugar
+    entre varios jugadores
+*/
+void info_de_juego();
+#endif // MODO_PRACTICA_H_INCLUDED
