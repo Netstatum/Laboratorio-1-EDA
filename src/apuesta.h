26f4b0a451c76381fe0f32322f76d4d2effc331f
Esteban Gaete <zoraskiller@gmail.com>
2012-01-07 21:43:38 -0300

    relativo a su .c

---
+++ src/apuesta.h
@@ -0,0 +1,44 @@
+#ifndef APUESTAS_H_INCLUDED
+#define APUESTA_H_INCLUDED
+
+#include "mesa.h"
+#include "jugador.h"
+#include "getch.h"
+
+/** Apuestas obligatorias al comienzo de cada ronda
+	Se aplican al primer y segundo jugandor correspondientemente
+ */
+int _low;
+int _big;
+
+/** Función apuestaG
+	Es la funcion general de apuestas, se asumen las siguientes cosas:
+		Sup0) La cantidad ingresada es valida (es decir es un int)
+		Sup1) La cantidad apostada es igual o mayor a la apuesta maxima (apuesta>=mesaJugando.apuesta_maxima)
+		Sup2) El jugador tiene el dinero suficiente para apostar lo que indica (apuesta<=jugando->dinero)
+ */
+void apuestaG(Jugador *jugando, int apuesta);
+
+/** Hacen el calculo de la apuesta maxima y minima obligatorias para los dos primeros jugadores
+    en la primera ronda de cada partida, este valor es fijo para el resto del juego
+	@param int mInicial: monto total inicial que tiene cada jugador al iniciar la partida
+ */
+void aLow(int mInicial);
+void aBig(int mInicial);
+
+/** Realizara las diferentes apuestas
+	@param Jugador *jugando : jugador apostando
+	       int apuesta: cantidad apostada
+	
+	Para realizar el low usar apostando(jugador,_low);
+	Para realizar el big usar apostando(jugador,_big);
+	Para igualar la apostando(jugador,mesaJugando.apuesta_maxima);
+	Para aumentar la apostando(jugador,mesaJugando.puesta_maxima+aumento);;
+	Para ir All In apostando(jugador,jugador->dinero);
+	
+	Si no tiene el dinero suficiente para realizar una de estas acciones, la apuesta realizada
+	sera equivalente a apostando(jugador,jugador->dinero);
+*/
+void apostando(Jugador *jugando, int apuesta);
+
+#endif
