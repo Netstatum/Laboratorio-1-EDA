f94cb4fab1820971fe685cfde23fb2111a66889c
Esteban Gaete <zoraskiller@gmail.com>
2012-01-08 00:04:04 -0300

    relativo a su .c

--- src/apuesta.h
+++ src/apuesta.h
@@ -11,10 +11,13 @@
 int _low;
 int _big;
 
+/** Si se debe realizar la apuesta obligatoria o no */
+int _aOB;
+
 /** Función apuestaG
 	Es la funcion general de apuestas, se asumen las siguientes cosas:
 		Sup0) La cantidad ingresada es valida (es decir es un int)
-		Sup1) La cantidad apostada es igual o mayor a la apuesta maxima (apuesta>=mesaJugando.apuesta_maxima)
+		Sup1) La cantidad apostada es igual o mayor a la apuesta maxima (apuesta>=mesaJuego.apuesta_maxima)
 		Sup2) El jugador tiene el dinero suficiente para apostar lo que indica (apuesta<=jugando->dinero)
  */
 void apuestaG(Jugador *jugando, int apuesta);
@@ -32,8 +35,8 @@ void aBig(int mInicial);
 	
 	Para realizar el low usar apostando(jugador,_low);
 	Para realizar el big usar apostando(jugador,_big);
-	Para igualar la apostando(jugador,mesaJugando.apuesta_maxima);
-	Para aumentar la apostando(jugador,mesaJugando.puesta_maxima+aumento);;
+	Para igualar la apostando(jugador,mesaJuego.apuesta_maxima);
+	Para aumentar la apostando(jugador,mesaJuego.puesta_maxima+aumento);;
 	Para ir All In apostando(jugador,jugador->dinero);
 	
 	Si no tiene el dinero suficiente para realizar una de estas acciones, la apuesta realizada
